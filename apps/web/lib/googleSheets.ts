d0e694ce390965676a6e54834379532f590483e7
Poltwas <poltwas7@gmail.com>
2025-10-31 17:34:13 +0100

    chore: force commit lib/googleSheets.ts

--- apps/web/lib/googleSheets.ts
+++ apps/web/lib/googleSheets.ts
@@ -1,6 +1,7 @@
 import { google } from 'googleapis';
 
 export function getSheetsClient() {
+	// Google Sheets client
 	const clientEmail = process.env.GOOGLE_SHEETS_CLIENT_EMAIL;
 	const privateKey = process.env.GOOGLE_SHEETS_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
